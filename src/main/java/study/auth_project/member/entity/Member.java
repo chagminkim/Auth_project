15c1b85e23aa79dddd4061a90efc3eadccf0fd0d
changminkim <cmk5724@naver.com>
2025-04-27 22:35:55 +0900

    feat:test

---
+++ src/main/java/study/auth_project/member/entity/Member.java
@@ -0,0 +1,4 @@
+package study.auth_project.member.entity;
+
+public class Member {
+}
---
+++ src/test/java/study/auth_project/AuthProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.auth_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
